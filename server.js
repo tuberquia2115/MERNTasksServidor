ffacbde65dda7dec37a9d7340fd256ad201e7345
SOLUTRAFFICDEVELOPERBETA <solutrafficdev002@gmail.com>
2020-03-22 19:35:32 -0500

    nueva confiracion

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(cors());
 // Habilitar express.json
 app.use(express.json({ extended: true }));
 // puerto de la app
-const port = process.env.port || 4000;
+const PORT = process.env.PORT || 5000;
 // Importar rutas
 app.use('/api/usuarios', require('./routes/usuarios'));
 app.use('/api/auth', require('./routes/auth'));
@@ -21,6 +21,6 @@ app.use('/api/proyectos', require('./routes/proyectos'));
 app.use('/api/tareas', require('./routes/tareas'));
 
 // arrancar la app
-app.listen(port, '0.0.0.0', () => {
-    console.log(`El servidor esta funcionando en el puerto ${port}`);
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`El servidor esta funcionando en el puerto ${PORT}`);
 })
